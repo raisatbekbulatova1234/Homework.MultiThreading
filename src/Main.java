371e13dc0dd972f1ec21c171f07f58760a197ceb
Raya <raisat.bekbulatova1234@gmail.com>
2022-08-23 23:38:13 +0300

    First commit.Task 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        ThreadGroup mainGroup = new ThreadGroup("main group");
+
+        System.out.println("Создание потоков");
+
+        Thread thread1 = new MyThread(mainGroup, "Поток 1");
+        Thread thread2 = new MyThread(mainGroup, "Поток 2");
+        Thread thread3 = new MyThread(mainGroup, "Поток 3");
+        Thread thread4 = new MyThread(mainGroup, "Поток 4");
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Завершение всех потоков");
+        mainGroup.interrupt();
+    }
+}
